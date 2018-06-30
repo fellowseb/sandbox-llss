dfe80d63d3f94565414b99033ce34d2d68f0048f
fellowseb <wauquier.sebastien@gmail.com>
2018-07-01 00:51:38 +0200

    Add chapter 5

--- 5_Variables.sh
+++ 5_Variables.sh
@@ -21,3 +21,54 @@ $@
 
 # When user logs in ~.profile is executed
 # When every single terminal is created, ~.bashrc is executed
+
+export MY_VAR=123 # Make it available in subshells
+# Example
+export TZ=America/Los_Angeles
+date
+unset TZ
+date
+
+readonly okay=9911 # Declare readonly variable
+declare -r okay=9911 # Idem
+
+# Positional parameters (options)
+set USA Canada France
+echo $1 $2 $3 ... ${10} ${11}
+options="USA Canada France"
+set $options # Does the same !
+
+# Use shift when reading parameters (<<)
+set One Two Three Four
+shift
+echo $1 # Two
+shift 2
+echo $1 # Four
+# Of course it affects $# $* $@
+
+# We can keep track of initial positional parameters before shifting:
+oldargs=$*
+set $oldargs # Restore
+
+# Use getopts to parse options
+# See getopt.sh
+
+# Default Parameters:
+MY_PARAM=${1:-default}
+echo $MY_PARAM
+
+# Arrays
+FRUITS=(Mango Banana Apple)
+# or
+declare -a FRUITS=(Mango Apple Lemon)
+echo ${FRUITS[*]}
+echo ${FRUITS[2]}
+FRUITS[3]=Orange
+echo ${FRUITS[*]}
+echo "Number of elements: ${#FRUITS[*]}"
+unset FRUITS # OR unset ${FRUITS[*]}
+
+# Some elements in the array can be empty
+country[4]=Japan
+country=(USA [2]=Japan [3]=Germany)
+
